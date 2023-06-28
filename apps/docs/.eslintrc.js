a791cf3d9e213c2ec4e97f548435be453b5881a2
Adam Wojtycki <adamwojtycki@gmail.com>
2023-06-28 14:46:33 +0200

    Update .eslintrc.js

--- apps/docs/.eslintrc.js
+++ apps/docs/.eslintrc.js
@@ -1,4 +1,14 @@
-module.exports = {
+/** @type {import('eslint').Linter.Config} */
+const config = {
   root: true,
-  extends: ["@acme/eslint-plugin"],
+  extends: [
+    "@acme/eslint-config/base",
+    "@acme/eslint-config/nextjs",
+    "@acme/eslint-config/react",
+  ],
+  parserOptions: {
+    project: "./tsconfig.json",
+  },
 };
+
+module.exports = config;
